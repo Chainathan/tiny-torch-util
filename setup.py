5fe37d152985ee754b8f0f0b9a042d06ab49abeb
Chainathan <70045165+Chainathan@users.noreply.github.com>
2024-03-28 18:06:59 -0400

    fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tiny-torch-utils',
     version='0.1',
     packages=find_packages(),
-    description='A brief description of what the package does',
+    description='A personal collection of small utility functions for PyTorch.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Chainathan',
